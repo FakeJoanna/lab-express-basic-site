9d67b241f2b238f9edf7d6c98e260fde97f2d21e
Brea Foga <breaardiente@gmail.com>
2023-02-12 18:14:01 +0100

    added necessary files and created and styled the home page

---
+++ app.js
@@ -0,0 +1,20 @@
+// Require Express
+const express = require('express');
+
+// Express server handling requests and responses
+const app = express();
+
+// Make everything inside of public/ available
+app.use(express.static('public'));
+
+// Make everything inside of views/ available
+app.use(express.static("views"));
+
+// home route:
+app.get('/home', (request, response, next) => response.sendFile(__dirname + '/views/home.html'));
+
+// aabout route:
+app.get('/about', (request, response, next) => response.sendFile(__dirname + '/views/about.html'));
+
+// Server Started
+app.listen(3000, () => console.log('app is listening'));
